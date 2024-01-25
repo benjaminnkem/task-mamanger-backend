ec4dddb8927549aa630b351c6f674c8b84c43177
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-01-26 00:34:00 +0100

    removed build folder

--- build/app.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authRoutes from "./routes/auth/controllers/auth.controller.js";
-const app = express();
-const PORT = 3000;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/api/auth", authRoutes);
-app.listen(PORT);
