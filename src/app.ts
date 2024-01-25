8c9f483e399352b15ebe740363656773b1499a15
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-01-26 00:56:51 +0100

    chore: swagger ui setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import authRoutes from "./routes/auth/controllers/auth.controller.js";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { options } from "./config/swagger-options.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const specs = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
 
 app.use("/api/auth", authRoutes);
 
